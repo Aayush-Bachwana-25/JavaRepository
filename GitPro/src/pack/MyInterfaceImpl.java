bfeec8f84740edda61c7e86d46e7a888206c4831
Aayush Bachwana <bachwana.aayush@gmail.com>
2023-01-31 15:43:48 +0530

    Another Commit

---
+++ GitPro/src/pack/MyInterfaceImpl.java
@@ -0,0 +1,10 @@
+package pack;
+
+public class MyInterfaceImpl implements MyInterface{
+	@Override
+	public void disp() {
+		// TODO Auto-generated method stub
+		System.out.println("Inside MyInterface IMPL");
+	}
+
+}
